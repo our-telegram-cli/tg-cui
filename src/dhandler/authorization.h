0923d46d6a5352a9d9e93bfc36bb7b7b7b4750db
dimonchik0036 <dimonchik0036@gmail.com>
2018-07-08 00:29:20 +0700

    [ FIX ] Fix guardian

--- src/dhandler/authorization.h
+++ src/dhandler/authorization.h
@@ -2,8 +2,8 @@
 // Created by dimonchik0036 on 07.07.18.
 //
 
-#ifndef TG_CUI_AUTH_H
-#define TG_CUI_AUTH_H
+#ifndef TG_CUI_AUTHARIZATION_H
+#define TG_CUI_AUTHARIZATION_H
 
 #include "interfaces/authorizable.h"
 
@@ -12,4 +12,4 @@ class Authorization : public Authorizable {
   void auth(std::string token) override;
 };
 
-#endif  // TG_CUI_AUTH_H
+#endif  // TG_CUI_AUTHARIZATION_H
